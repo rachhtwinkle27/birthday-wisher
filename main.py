6e4a1a8e7248295c85692a1b7a527b1ef2a68e70
rachhtwinkle27 <rachhtwinkle27@gmail.com>
2026-07-16 21:58:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,26 @@
 import random
 import pandas
 import smtplib
-import datetime as dt
+from datetime import datetime
+import os
 
-test_email = "testing.python5394@gmail.com"
-test_password = "lemkrwrghzfycjdh"
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-
-# ---------------------- Current date ------------------------#
 today = dt.datetime.now()
 today_tuple = (today.month, today.day)
 
-# ------------------------ Read CSV ------------------------#
-
 data_file = pandas.read_csv("birthdays.csv")
 
-# ------------------------ Dictionary comprehension to create dictionary from birthdays.csv ------------------#
-
 birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data_file.iterrows()}
-
-# ------------------------------- Check if today's month/day tuple matches one of the key in birthdays.csv-------------#
 
 if today_tuple in birthday_dict:
     birthday_person = birthday_dict[today_tuple]
 
-    # ------------------------- Random letters ---------------------#
-
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-
-    # ----------------------- Read letters ---------------------- #
 
     with open(file_path) as file:
         letter_content = file.read()
-
-        # -------------------- Replace name --------------------- #
 
         letter_content = letter_content.replace("[NAME]", birthday_person["name"])
 
@@ -46,4 +33,6 @@ if today_tuple in birthday_dict:
                 msg=f"Subject:Happy Birthday!\n\n{letter_content}"
             )
 
-    print("Mail sent")
+    print("Birthday email sent successfully")
+else:
+    print("No birthdays today")
