7af45ae08ddd651f99c92bd8b0077ae70f0ccb48
rachhtwinkle27 <rachhtwinkle27@gmail.com>
2026-07-16 22:25:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ import os
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = dt.datetime.now()
+today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data_file = pandas.read_csv("birthdays.csv")
 
-birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data_file.iterrows()}
+birthday_dict = {
+    (data_row["month"], data_row["day"]): data_row 
+    for (index, data_row) in data_file.iterrows()
+}
 
 if today_tuple in birthday_dict:
     birthday_person = birthday_dict[today_tuple]
@@ -24,14 +27,14 @@ if today_tuple in birthday_dict:
 
         letter_content = letter_content.replace("[NAME]", birthday_person["name"])
 
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(user=test_email, password=test_password)
-            connection.sendmail(
-                from_addr=test_email,
-                to_addrs=birthday_person["email"],
-                msg=f"Subject:Happy Birthday!\n\n{letter_content}"
-            )
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{letter_content}"
+        )
 
     print("Birthday email sent successfully")
 else:
